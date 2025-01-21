53903b20328260cba51a468edae432ea53bf75f3
Daniel Bucket <danieljbucket@gmail.com>
2025-01-21 13:31:03 -0700

    service area map loading with polylines showing service area boundaries

---
+++ src/components/Main/ServiceArea/ServiceAreaMap.jsx
@@ -0,0 +1,50 @@
+import { Loader } from '@googlemaps/js-api-loader';
+import icon from '../../../assets/images/tree_logo.png';
+import { mapCenter, goldenCoordinates, mapAreaCoordinates } from './mapData';
+
+export default function ServiceAreaMap() {
+  const loader = new Loader({
+    apiKey: import.meta.env.VITE_GMAPS_API,
+    version: 'weekly',
+  })
+
+  const markerIcon = document.createElement('img');
+    markerIcon.src = icon;
+    markerIcon.alt="tree logo";
+    markerIcon.style.height = "5em";
+    markerIcon.style.width = "5em";
+
+  loader.load().then(async () => {
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    const map = new Map(document.getElementById('map'), {
+      center: mapCenter,
+      zoom: 10,
+      mapId: "GOLDEN_CO_MAP",
+      disableDefaultUI: true,
+    });
+
+    const serviceArea = new google.maps.Polygon({
+      path: mapAreaCoordinates,
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+    });
+
+    const marker = new AdvancedMarkerElement({
+      position: goldenCoordinates,
+      map: map,
+      title: 'Golden, Colorado',
+      content: markerIcon
+    });
+
+    serviceArea.setMap(map);
+  });
+
+  return (
+    <div id="map" className="map-container"></div>
+  );
+};
--- src/components/Main/ServiceArea/index.jsx
+++ src/components/Main/ServiceArea/index.jsx
@@ -1,27 +1,14 @@
 import './index.style.css'
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
-const GmapsKey = import.meta.env.VITE_GMAPS_API
-import { mapCenter, goldenCoordinates, mapAreaCoordinates } from './mapData'
+import ServiceAreaMap from './ServiceAreaMap'
 
 export default function ServiceArea() {
-  const statement = `Serving Golden, Colorado, and Denver's western 'burbs`
 
   return (
     <div className='service-area-container card-body'>
       <p className='component-header'>Service Area</p>
       <div className="map-container">
         <p>Serving Golden, Colorado <br/>& Denvers western burbs.</p>
-        <APIProvider
-          apiKey={GmapsKey}
-          onLoad={() => console.log('maps loaded')}>
-          <Map className='google-map'
-            defaultZoom={13}
-            defaultCenter={mapCenter}
-            onCameraChanged={(ev) =>
-              console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-            }>
-          </Map>
-        </APIProvider>
+        <ServiceAreaMap />
       </div>
     </div>
   )
