1f16ccc84f84d014057d779febd83e792ab916d2
Daniel Bucket <danieljbucket@gmail.com>
2025-01-21 09:15:37 -0700

    fix email address link; the address was spelled incorrectly

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import './index.style.css'
 import logo from '../../assets/images/arborcraft.png'
-const mailto = 'mailto:daniel@aborcrafttreeservice.com'
+const mailto = 'mailto:daniel@arborcrafttreeservice.com'
 const phonenumber = '303-847-5786'
 
 export default function Header() {
