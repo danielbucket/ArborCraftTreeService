afb79f4d6294577698e250d218136ce880c4de68
Daniel Bucket <danieljbucket@gmail.com>
2026-03-08 14:01:44 -0600

    remove phone number

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,6 @@ import phoneIcon from '../../assets/images/phone.png';
 import copyIcon from '../../assets/images/clipboards.png';
 
 const email = 'daniel@arborcraftservices.com'
-const phonenumber = '720-713-8773'
 
 export default function Header() {
   const handleClick = (e) => {
@@ -27,10 +26,6 @@ export default function Header() {
           <img src={logo} alt='ArborCraft logo' />
         </div>
         <div className='contact-container'>
-          <div className='contact-content phone-wrapper'>
-            <p>{phonenumber}</p>
-            <img src={phoneIcon}/>
-          </div>
           <div className='contact-content email-wrapper'>
             <p>{email}</p>
             <img className='copy-icon' src={copyIcon} onClick={handleClick}/>
